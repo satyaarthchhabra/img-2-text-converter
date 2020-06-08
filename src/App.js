fe28ebcd6d8269c683a4e8c3eb2f99b64f401c0b
Satyaarth <satyarthchhabra18@gmail.com>
2020-06-08 18:01:10 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Tesseract from "tesseract.js";
+import TextWrapper from "./components/TextWrapper";
+import ImageWrapper from "./components/ImageWrapper";
+import axios from "axios";
+
+// create account on imgbb and go to > about section > and get that api key and put it here
+
+const API_KEY = "fff3dd87362ba409b9aa714370c54492";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [text, setText] = useState(null);
+
+  const convertImageToText = async () => {
+    setLoading(true);
+    const result = await Tesseract.recognize(imageUrl, "eng");
+    setText(result.data.text);
+    setLoading(false);
+  };
+
+  const uploadFile = async e => {
+    try {
+      setLoading(true);
+      const formData = new FormData();
+      formData.append("image", e.target.files[0]);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+
+      const res = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${API_KEY}`,
+        formData,
+        config
+      );
+      setImageUrl(res.data.data.url);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (imageUrl != null) {
+      convertImageToText();
+    }
+  }, [imageUrl]);
+  console.log(`${process.env.REACT_APP_API_KEY}`);
+  return (
+    <div className="App">
+      <img src="https://i.ibb.co/LpxDvR1/logo.png" className="logo" alt="" />
+      <div className="container">
+        {loading && <div className="loader"></div>}
+        {text == null ? (
+          <ImageWrapper loading={loading} uploadFile={uploadFile} />
+        ) : (
+          <TextWrapper text={text} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
